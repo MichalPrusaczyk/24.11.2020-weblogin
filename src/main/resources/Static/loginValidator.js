39e43917a85dea29ea67e84e19933931fd901f7c
Michal Prusaczyk <michal@prusaczyk.eu>
2020-11-25 09:19:48 +0100

    24.11.2020 - Start adding JavaScript.

--- src/main/resources/Static/loginValidator.js
+++ src/main/resources/Static/loginValidator.js
@@ -2,12 +2,16 @@ function validate(){
     var login = document.forms["loginForm"]["login"].value;
     var pass = document.forms["loginForm"]["pass"].value;
 
+        var flag = true;
+
        if(login == ""){
               document.forms["loginForm"]["login"].style.background = "red";
-            return false;
+                flag = false;
        }
        if(pass == ""){
               document.forms["loginForm"]["login"].style.background = "red";
-            return false;
+                flag = false;
        }
+
+       return flag;
 }
